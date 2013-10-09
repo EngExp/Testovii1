adcd39c1fcdd656b084e3109b8b46f742f639871
Egor Abramenko <EngExp@users.noreply.github.com>
2013-10-09 12:50:09 +0700

    Update naturaldoc.bat

--- naturaldoc.bat
+++ naturaldoc.bat
@@ -1,5 +1,1 @@
-C:\Users\egor.abramenko\Documents\temp\NaturalDocs-1.52\NaturalDocs.bat 
-			-i C:\Users\egor.abramenko\Documents\temp\github\test aviasales\mint-master\mint-master
-            -o FramedHTML C:\Users\egor.abramenko\Documents\temp\github\test aviasales\mint-master\mint-master\doc
-            -p C:\Users\egor.abramenko\Documents\temp\NaturalDocs-1.52
-pause
+NaturalDocs -i ./lib/ -o HTML ./doc/ -p ./nd_internal/
