eef2540b0c5725b208ea26e2ea7868db59bdc8d6
engexp <info@engexp.ru>
2013-10-09 12:44:13 +0700

    tot htp

---
+++ naturaldoc.bat
@@ -0,0 +1,5 @@
+C:\Users\egor.abramenko\Documents\temp\NaturalDocs-1.52\NaturalDocs.bat 
+			-i C:\Users\egor.abramenko\Documents\temp\github\test aviasales\mint-master\mint-master
+            -o FramedHTML C:\Users\egor.abramenko\Documents\temp\github\test aviasales\mint-master\mint-master\doc
+            -p C:\Users\egor.abramenko\Documents\temp\NaturalDocs-1.52
+pause
